cda5e209d676211092210b0ad65a045db2465e23
Michal Nowotka <mnowotka@ebi.ac.uk>
2017-01-11 10:26:31 +0000

    corrections in setup

--- setup.py
+++ setup.py
@@ -12,14 +12,17 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
+if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 7):
+    raise Exception('ChEMBL software stack requires python 2.7.3 - 2.7.7')
+
 setup(
-    name='chembl_api',
-    version='0.5.16',
+    name='chembl-api',
+    version='0.8.1',
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
     description='Python package providing RESTful chembl API.',
     url='https://www.ebi.ac.uk/chembldb/index.php/ws',
-    license='Apache Software License',
+    license='CC BY-SA 3.0',
     packages=['chembl_api'],
     long_description=open('README.rst').read(),
     install_requires=[
@@ -27,14 +30,14 @@ setup(
         'simplejson==2.3.2',
         'defusedxml>=0.4.1',
         'django-tastypie==0.10',
-        'chembl_business_model>=0.5.3'
+        'chembl-business-model>=0.8.1'
     ],
     include_package_data=False,
     classifiers=['Development Status :: 2 - Pre-Alpha',
                  'Environment :: Web Environment',
                  'Framework :: Django',
                  'Intended Audience :: Developers',
-                 'License :: OSI Approved :: Apache Software License',
+                 'License :: Creative Commons :: Attribution-ShareAlike 3.0 Unported',
                  'Operating System :: POSIX :: Linux',
                  'Programming Language :: Python :: 2.7',
                  'Topic :: Scientific/Engineering :: Chemistry'],
